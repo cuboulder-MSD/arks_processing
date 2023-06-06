46a64bad95db8cb20270ab79eef58cae7462acf7
University of Colorado Boulder Libraries - Metadata Production & Curation Services <47370990+cuboulder-MSD@users.noreply.github.com>
2023-06-06 10:37:31 -0600

    Create skeleton_updateARKs.py

---
+++ skeleton_updateARKs.py
@@ -0,0 +1,22 @@
+#import libraries
+
+
+
+
+
+
+
+
+
+#query ark and update with new url
+def changeARK
+
+
+
+
+
+
+
+
+#open csv and find columns you want. may use a second function to look for variations on ARK columns
+def main
